0a1c215eb2a75f50a54ba63e38d25f7b6841df2c
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-02-10 14:36:26 +0200

    Make info page and features to show individual contacts, add contacts and delete contacts

--- index.js
+++ index.js
@@ -24,12 +24,57 @@ let persons = [
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send(persons)
+app.use(express.json())
+
+app.get('/api/info', (request, response) => {
+  const date = new Date();
+
+  response.send(`Phonebook has info of ${persons.length} people ${date}`)
+  response.json(date)
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(persons)
+app.get('/', (request, response) => {
+  response.send(`Frontpage`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  // person.id = persons.length + 1
+  const randomNumber = Math.random() * (9999999999 - 1000000000) + 1000000000
+  personNumber = randomNumber.toString()
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: personNumber,
+    id: persons.length + 1
+  }
+  
+  persons = persons.concat(person)
+  console.log(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
 const PORT = 3001
