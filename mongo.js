6618f7c0014daa8ca11d832a4bed579c86352eab
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-07-03 14:30:25 +0300

    Fix mongo connection

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length<3 ) {
+if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -8,36 +8,25 @@ if ( process.argv.length<3 ) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://sofia:${password}@cluster0-zqlrn.mongodb.net/note-app?retryWrites=true&w=majority`
+  `mongodb+srv://sofia:${password}@cluster0-zqlrn.mongodb.net/phonebook?retryWrites=true`
+
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+.catch(error => console.log(error))
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
-  id: Number
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
+const newContact = new Person({
   name: process.argv[3],
   number: process.argv[4]
 })
 
-if (process.argv.length <= 3) {
-  Person.find({}).then(result => {
-    result.forEach(person => {
-      console.log(person)
-    })
-    mongoose.connection.close()
-  })
-}
-else {
-  person.save().then(response => {
-    console.log(person.name)
-    console.log(person.number)
-    console.log('contact added!');
-    mongoose.connection.close();
-  })
-}
+newContact.save().then(response => {
+  console.log('new contact saved!')
+  mongoose.connection.close()
+})
