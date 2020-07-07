dce04cb386f33836aa71ce0e01e5eaf762981ef8
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-07-07 18:51:32 +0300

    Move error handlers to middlewares

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/', (request, response) => {
   response.send(`Frontpage`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
       response.json(person)
@@ -62,10 +62,7 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-    })
+  .catch(error => next(error))
   })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -78,7 +75,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   
   const body = request.body
   
@@ -95,10 +92,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedContact => {
     response.json(savedContact.toJSON())
   })
-  .catch(error => {
-    console.log(error)
-    response.status(404).end()
-  })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
@@ -116,15 +110,30 @@ app.put('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
-  .catch(error => {
-    console.log(error)
-    response.status(404).end()
-  })
+  .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
