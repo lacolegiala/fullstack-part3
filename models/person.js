bb3159d5208827d2b776a8a189d17f6278eab0e1
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-07-08 17:53:54 +0300

    Add data validations

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-// const password = process.env.password
+const uniqueValidator = require('mongoose-unique-validator');
+mongoose.set('useCreateIndex', true);
 
 const url = process.env.MONGODB_URI
 
@@ -11,8 +11,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch(error => console.log('Error while connecting', error))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {type: String, required: true, unique: true},
+  number: {type: String, required: true}
 })
 
 personSchema.set('toJSON', {
@@ -22,5 +22,7 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
